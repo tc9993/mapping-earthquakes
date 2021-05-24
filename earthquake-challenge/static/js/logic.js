1d1566fb5762d33cda2cd8be1c5574d183a58e61
tc9993 <taylorbcoffee@gmail.com>
2021-05-24 13:19:33 -0500

    mod 13 challenge

---
+++ earthquake-challenge/static/js/logic.js
@@ -0,0 +1,274 @@
+// // Add consol.log to check if code works
+// console.log('working');
+
+// Create tile layer that is the background
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+let baseMaps = {
+    Street: streets,
+    Light: light,   
+    Dark: dark,
+    Satellite: satelliteStreets
+};
+
+//create eq layer for our map
+let earthquakes = new L.LayerGroup();
+
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+    Earthquakes: earthquakes
+};
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid', {
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
+});
+
+// Then we add a control to the map that will allow the user to change
+// which layers are visible.
+L.control.layers(baseMaps, overlays).addTo(map);
+
+// //pass our map layers into our layers control and add the layers control to the map
+// L.control.layers(baseMaps).addTo(map);
+
+////Accessing the airpor geojson url
+//let airportData = 'https://raw.githubusercontent.com/tc9993/mapping-earthquakes/fb81d06c4d9d395d92009c1e4df3d99ecb6a9fef/Lessonwork/mapping-geojson-points/static/js/majorAirports.json';
+//let torontoData = 'https://raw.githubusercontent.com/tc9993/mapping-earthquakes/mapping-linestrings/Lessonwork/mapping-linestrings/static/js/torontoRoutes.json';
+//let torontoHoods = 'https://raw.githubusercontent.com/tc9993/mapping-earthquakes/mapping-polygons/Lessonwork/mapping-polygons/static/js/torontoNeighborhoods.json';
+let eqData = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
+
+
+//returns style data for each eq we plot, must pass magnitude into function
+function styleInfo(feature){
+    return{
+        opacity: 1,
+        fillOpacity: 1,
+        fillColor: getColor(feature.properties.mag),
+        color: "#000000",
+        radius: getRadius(feature.properties.mag),
+        stroke: true,
+        weight: 0.5
+    };
+}
+
+// This function determines the color of the circle based on the magnitude of the earthquake.
+function getColor(magnitude) {
+    if (magnitude > 5) {
+      return "#ea2c2c";
+    }
+    else if (magnitude > 4) {
+      return "#ea822c";
+    }
+    else if (magnitude > 3) {
+      return "#ee9c00";
+    }
+    else if (magnitude > 2) {
+      return "#eecc00";
+    }
+    else if (magnitude > 1) {
+      return "#d4ee00";
+    }
+    else {
+        return "#98ee00";
+    }
+}
+
+function getRadius(magnitude){
+    if(magnitude === 0){
+        return 1
+    }
+    return magnitude * 4;
+}
+
+// Create a legend control object.
+let legend = L.control({
+    position: "bottomright"
+});
+
+// Then add all the details for the legend.
+legend.onAdd = function() {
+    let div = L.DomUtil.create("div", "info legend");
+    const magnitudes = [0, 1, 2, 3, 4, 5];
+    const colors = [
+    "#98ee00",
+    "#d4ee00",
+    "#eecc00",
+    "#ee9c00",
+    "#ea822c",
+    "#ea2c2c"
+    ];
+
+    // Looping through our intervals to generate a label with a colored square for each interval.
+    for (var i = 0; i < magnitudes.length; i++) {
+        console.log(colors[i]);
+        div.innerHTML +=
+        "<i style='background: " + colors[i] + "'></i> " +
+        magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+    }
+    return div; 
+};
+
+legend.addTo(map);
+
+//grabbing our GeoJSON data
+d3.json(eqData).then(function(data){
+    console.log(data);
+    //create geojson layer w/ data
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng){
+            console.log(data);
+            return L.circleMarker(latlng);
+        },
+        style: styleInfo,
+        //create popup for each marker to display mag and locale of eq
+        onEachFeature: function(feature,layer){
+            layer.bindPopup('Magnitude: ' + feature.properties.mag + "<br>Location: " + feature.properties.place);
+        }
+    }).addTo(earthquakes);
+
+    earthquakes.addTo(map);
+});
+
+
+// //grabbing our GeoJSON data
+// d3.json(eqData).then(function(data){
+//     console.log(data);
+//     //create geojson layer w/ data
+//     L.geoJson(data, {
+//         style: myStyle,
+//         onEachFeature: function(feature, layer){
+//             layer.bindPopup('<h3> Airline: ' + feature.properties.airline + '</h3> <hr><h3> Destination: ' + feature.properties.dst + '</h3>');
+//         }
+//     }
+//     ).addTo(map)
+// });
+
+//-----------------------------------------------------------------------13.5.4 ^
+
+// let myStyle = {
+//     color: '#ffffa1',
+//     weight:2
+// }
+
+// //add our 'graymap' tile layer to the map
+// streets.addTo(map);
+
+// //Accessing the airpor geojson url
+// let airportData = 'https://raw.githubusercontent.com/tc9993/mapping-earthquakes/fb81d06c4d9d395d92009c1e4df3d99ecb6a9fef/Lessonwork/mapping-geojson-points/static/js/majorAirports.json';
+
+// //grabbing geojson data
+// d3.json(airportData).then(function(data){
+//     console.log(data);
+//     //creating a geojson layer with the retrieved data
+//     L.geoJson(data).addTo(map);
+// });
+
+// //  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// let marker = L.circle([34.0522, -118.2437], {
+//     radius: 100
+//  }).addTo(map);
+
+// let marker = L.circleMarker([34.0522, -118.2437]).addTo(map);
+
+//get data from cities.js
+// let cityData = cities;
+
+// cityData.forEach(function(city){
+//     console.log(city);
+//     L.circleMarker(city.location, {
+//         radius: city.population / 100000
+//     })
+//     .bindPopup('<h2>' + city.city + ', ' + city.state + '</h2> <hr> <h3>Population ' + city.population.toLocaleString() + "</h3>")
+//     .addTo(map);
+// });
+
+// let line = [
+//     [33.9416, -118.4085],
+//     [30.1975, -97.6664],
+//     [43.6777, -79.6248],
+//     [40.6413, -73.7781]
+// ];
+
+// // Create a polyline using the line coordinates and make the line red.
+// L.polyline(line, {
+//     dashArray: "30 10",
+//     color: "blue"
+//   }).addTo(map);
+
+// //Add GeoJSON data
+// let sanFranAirport =
+// {
+//     'type':'FeatureCollection', 'features':[{
+//         'type':'Feature',
+//         'properties':{
+//             'id':'3469',
+//             "name":"San Francisco International Airport",
+//             "city":"San Francisco",
+//             "country":"United States",
+//             "faa":"SFO",
+//             "icao":"KSFO",
+//             "alt":"13",
+//             "tz-offset":"-8",
+//             "dst":"A",
+//             "tz":"America/Los_Angeles"},
+//             "geometry":{
+//                 "type":"Point",
+//                 "coordinates":[-122.375,37.61899948120117]}
+    
+//     }]
+// }
+
+// // Grabbing our GeoJSON data. 13.5.1
+// L.geoJSON(sanFranAirport).addTo(map);
+// L.geoJSON(sanFranAirport, {
+//     //turn each feature into a marker on the map
+//     pointToLayer: function(feature, latlng){
+//         console.log(feature);
+//         return L.marker(latlng)
+//         .bindPopup('<h2>' + feature.properties.city + '</h2>' + '<h>' + feature.properties.city + ', ' + feature.properties.country)
+//     }
+// }).addTo(map);
+
+// //grabbing geosjon data THIS ONE IS 13.5.2  - THESE DONT SEEM TO WORK??
+// L.geoJson(airportData, {
+//     onEachFeature: function(feature, layer) {
+//         return L.marker(layer)
+//         .bindPopup();
+//      }
+// });
+
+// L.geoJSON(airportData, {
+//     onEachFeature: function(feature,layer){
+//         console.log(layer);
+//         layer.bindPopup();
+//     }
+// }).addTo(map);
